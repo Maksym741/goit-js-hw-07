a5ebb48467f1613f6610555f1f6fe3ff684e31a7
Maksym741 <pastushinamaksim@gmail.com>
2024-04-14 21:24:42 +0200

    .

--- js/task-6.js
+++ js/task-6.js
@@ -1,38 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, "0")}`;
+    .padStart(6, 0)}`;
 }
-
-const createBtn = document.querySelector("[data-create]");
-const destroyBtn = document.querySelector("[data-destroy]");
-const boxesContainer = document.getElementById("boxes");
-
+const input = document.querySelector('input[type="number"]');
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const boxesDiv = document.querySelector("#boxes");
 createBtn.addEventListener("click", createBoxes);
 destroyBtn.addEventListener("click", destroyBoxes);
-
 function createBoxes() {
-  const amount = +document.querySelector("input").value;
-
+  const amount = input.value;
   if (amount < 1 || amount > 100) {
     alert("Please enter a number between 1 and 100.");
     return;
   }
-
-  boxesContainer.innerHTML = "";
-
+  const boxes = [];
   for (let i = 0; i < amount; i++) {
     const box = document.createElement("div");
     box.classList.add("box");
-    box.style.backgroundColor = getRandomHexColor();
     box.style.width = `${30 + i * 10}px`;
     box.style.height = `${30 + i * 10}px`;
-    boxesContainer.appendChild(box);
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
   }
-
-  document.querySelector("input").value = "";
+  boxesDiv.innerHTML = "";
+  boxesDiv.append(...boxes);
+  input.value = "";
 }
-
 function destroyBoxes() {
-  boxesContainer.innerHTML = "";
+  boxesDiv.innerHTML = "";
 }
